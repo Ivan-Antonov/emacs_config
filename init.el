25781fa00ab2f0433b1fcba1516ffb4ef8751544
Ivan Antonov <ivan@bryans-MacBook-Pro.local>
2019-10-31 16:16:32 +0000

    Second Commit

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
       (interactive)
       (if (eolp) (let (parens-require-spaces) (insert-pair)) (self-insert-command 1)))
 
-(org-babel-load-file (expand-file-name '"/Users/Ivanantonov/.emacs.d/config.org"))
+(org-babel-load-file (expand-file-name '"/Users/ivan/.emacs.d/config.org"))
 (setq default-frame-alist '((font . "Monaco-12")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -31,7 +31,8 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (yasnippet-snippets yasnippet smex ido-vertical-mode ideo-vertical-mode org-bullets beacon spacemacs-theme which-key use-package usepackage))))
+    (magithub magit yasnippet-snippets yasnippet smex ido-vertical-mode ideo-vertical-mode org-bullets beacon spacemacs-theme which-key use-package usepackage))))
+(global-set-key (kbd "C-x g") 'magit-status)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
